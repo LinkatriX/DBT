acace13b3bc1827b570646f7091f285f76e6bcc6
NuovaPrime <loganman51@hotmail.com>
2019-07-26 15:57:14 -0600

    Unfucked the csproj

--- DBTMod.cs
+++ DBTMod.cs
@@ -20,163 +20,164 @@ using DBT.UserInterfaces.KiAttackUI;
 
 namespace DBT
 {
-	public sealed class DBTMod : Mod
-	{
-	  internal ModHotKey characterMenuKey, energyChargeKey, transformDownKey, speedToggleKey, transformUpKey, flightToggleKey, instantTransmission;
-		internal KiBar kiBar;
-		internal UserInterface kiBarInterface;
+    public sealed class DBTMod : Mod
+    {
+        internal ModHotKey characterMenuKey, energyChargeKey, transformDownKey, speedToggleKey, transformUpKey, flightToggleKey, instantTransmission;
 
-		internal OverloadBar overloadBar;
-		internal UserInterface overloadBarInterface;
+        internal KiBar kiBar;
+        internal UserInterface kiBarInterface;
 
-		internal DBTMenu dbtMenu;
-		internal CharacterTransformationsMenu characterTransformationsMenu;
-		internal UserInterface characterMenuInterface;
-		internal static CircleShader circle;
+        internal static CircleShader circle;
+        internal KiBrowserUIMenu kiBrowserMenu;
+        internal UserInterface kiBrowserInterface;
+        internal OverloadBar overloadBar;
+        internal UserInterface overloadBarInterface;
+        internal DBTMenu dbtMenu;
+        internal CharacterTransformationsMenu characterTransformationsMenu;
+        internal UserInterface characterMenuInterface;
 
-		public DBTMod()
-		internal static CircleShader circle;
-		internal KiBrowserUIMenu kiBrowserMenu;
-		internal UserInterface kiBrowserInterface;
+        public DBTMod()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true,
+                AutoloadBackgrounds = true
+            };
 
-		public DBTMod()
-		{
-			Properties = new ModProperties()
-			{
-				Autoload = true,
-				AutoloadGores = true,
-				AutoloadSounds = true,
-				AutoloadBackgrounds = true
-			};
+            Instance = this;
+        }
 
-			Instance = this;
-		}
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                SteamHelper.Initialize();
 
-		#region Load/Unloading
+                #region HotKeys
 
-		public override void Load()
-		{
-			if (!Main.dedServ)
-			{
-				SteamHelper.Initialize();
+                characterMenuKey = RegisterHotKey("Character Menu", "K");
+                energyChargeKey = RegisterHotKey("Energy Charge", "C");
+                speedToggleKey = RegisterHotKey("Speed Toggle", "Z");
+                transformDownKey = RegisterHotKey("Transform Down", "V");
+                transformUpKey = RegisterHotKey("Transform Up", "X");
+                flightToggleKey = RegisterHotKey("Flight Toggle", "Q");
+                instantTransmission = RegisterHotKey("Instant Transmission", "I");
 
-				#region HotKeys
+                #endregion
 
-				characterMenuKey = RegisterHotKey("Character Menu", "K");
-				energyChargeKey = RegisterHotKey("Energy Charge", "C");
-				speedToggleKey = RegisterHotKey("Speed Toggle", "Z");
-				transformDownKey = RegisterHotKey("Transform Down", "V");
-				transformUpKey = RegisterHotKey("Transform Up", "X");
-				flightToggleKey = RegisterHotKey("Flight Toggle", "Q");
-				instantTransmission = RegisterHotKey("Instant Transmission", "I");
+                dbtMenu = new DBTMenu();
+                dbtMenu.Activate();
 
-				#endregion
+                kiBrowserMenu = new KiBrowserUIMenu();
+                kiBrowserMenu.Activate();
+                kiBrowserInterface = new UserInterface();
+                kiBrowserInterface.SetState(kiBrowserMenu);
 
-				dbtMenu = new DBTMenu();
-		dbtMenu.Activate();
+                characterTransformationsMenu = new CharacterTransformationsMenu(this);
+                characterTransformationsMenu.Activate();
+                characterMenuInterface = new UserInterface();
+                characterMenuInterface.SetState(characterTransformationsMenu);
 
-				kiBrowserMenu = new KiBrowserUIMenu();
-		kiBrowserMenu.Activate();
-				kiBrowserInterface = new UserInterface();
-		kiBrowserInterface.SetState(kiBrowserMenu);
+                Properties = new ModProperties()
+                {
+                    Autoload = true,
+                    AutoloadGores = true,
+                    AutoloadSounds = true,
+                    AutoloadBackgrounds = true
+                };
 
-				characterTransformationsMenu = new CharacterTransformationsMenu(this);
-		characterTransformationsMenu.Activate();
-				characterMenuInterface = new UserInterface();
-		characterMenuInterface.SetState(characterTransformationsMenu);
+                Instance = this;
+                #region Load/Unloading
 
+                circle = new CircleShader(new Ref<Effect>(GetEffect("Effects/CircleShader")), "Pass1");
 
-				circle = new CircleShader(new Ref<Effect>(GetEffect("Effects/CircleShader")), "Pass1");
-			}
-	kiBrowserMenu.Visible = true;
-		}
-	}
+                kiBrowserMenu.Visible = true;
+            }
+        }
 
-	public override void Unload()
-{
-	if (!Main.dedServ)
-	{
-		kiBar.Visible = false;
+        public override void Unload()
+        {
+            if (!Main.dedServ)
+            {
+                kiBar.Visible = false;
 
-		kiBrowserMenu.Visible = false;
+                kiBrowserMenu.Visible = false;
 
-		characterTransformationsMenu.Visible = false;
+                characterTransformationsMenu.Visible = false;
 
-		overloadBar.Visible = false;
-	}
+                overloadBar.Visible = false;
+            }
 
-	Instance = null;
-}
+            Instance = null;
+        }
 
-#endregion
+        #endregion
 
-public override void UpdateUI(GameTime gameTime)
-{
-	if (characterMenuInterface != null && characterTransformationsMenu.Visible)
-		characterMenuInterface.Update(gameTime);
+        public override void UpdateUI(GameTime gameTime)
+        {
+            if (characterMenuInterface != null && characterTransformationsMenu.Visible)
+                characterMenuInterface.Update(gameTime);
 
-	if (kiBrowserInterface != null && kiBrowserMenu.Visible)
-		kiBrowserInterface.Update(gameTime);
+            if (kiBrowserInterface != null && kiBrowserMenu.Visible)
+                kiBrowserInterface.Update(gameTime);
 
-}
+        }
 
-[Obsolete]
-public override void UpdateMusic(ref int music)
-{
-	int[] noOverride =
-		{
-					MusicID.Boss1, MusicID.Boss2, MusicID.Boss3, MusicID.Boss4, MusicID.Boss5,
-					MusicID.LunarBoss, MusicID.PumpkinMoon, MusicID.TheTowers, MusicID.FrostMoon, MusicID.GoblinInvasion,
-					MusicID.Eclipse, MusicID.MartianMadness, MusicID.PirateInvasion,
-					GetSoundSlot(SoundType.Music, "Sounds/Music/TheUnexpectedArrival"),
-				};
+        [Obsolete]
+        public override void UpdateMusic(ref int music)
+        {
+            int[] noOverride =
+                {
+                    MusicID.Boss1, MusicID.Boss2, MusicID.Boss3, MusicID.Boss4, MusicID.Boss5,
+                    MusicID.LunarBoss, MusicID.PumpkinMoon, MusicID.TheTowers, MusicID.FrostMoon, MusicID.GoblinInvasion,
+                    MusicID.Eclipse, MusicID.MartianMadness, MusicID.PirateInvasion,
+                    GetSoundSlot(SoundType.Music, "Sounds/Music/TheUnexpectedArrival"),
+                };
 
-	int m = music;
-	bool playMusic = !noOverride.Any(song => song == m) || !Main.npc.Any(npc => npc.boss);
+            int m = music;
+            bool playMusic = !noOverride.Any(song => song == m) || !Main.npc.Any(npc => npc.boss);
 
-	Player player = Main.LocalPlayer;
+            Player player = Main.LocalPlayer;
 
-	if (player.active && player.GetModPlayer<DBTPlayer>(this).zoneWasteland && !Main.gameMenu && playMusic)
-	{
-		music = GetSoundSlot(SoundType.Music, "Sounds/Music/Wastelands");
-	}
-}
+            if (player.active && player.GetModPlayer<DBTPlayer>(this).zoneWasteland && !Main.gameMenu && playMusic)
+            {
+                music = GetSoundSlot(SoundType.Music, "Sounds/Music/Wastelands");
+            }
+        }
 
-public override void HandlePacket(BinaryReader reader, int whoAmI) => NetworkPacketManager.Instance.HandlePacket(reader, whoAmI);
+        public override void HandlePacket(BinaryReader reader, int whoAmI) => NetworkPacketManager.Instance.HandlePacket(reader, whoAmI);
 
-public override void PostSetupContent()
-{
-	// Boss checklist support
-	Mod bossChecklist = ModLoader.GetMod("BossChecklist");
-	if (bossChecklist != null)
-	{
-		bossChecklist.Call("AddBossWithInfo", "A Frieza Force Ship", 3.8f, (Func<bool>)(() => DBTWorld.downedFriezaShip), "Alert and let a frieza force scout escape in the wasteland biome after the world evil has been killed.");
-	}
-}
+        public override void PostSetupContent()
+        {
+            // Boss checklist support
+            Mod bossChecklist = ModLoader.GetMod("BossChecklist");
+            if (bossChecklist != null)
+            {
+                bossChecklist.Call("AddBossWithInfo", "A Frieza Force Ship", 3.8f, (Func<bool>)(() => DBTWorld.downedFriezaShip), "Alert and let a frieza force scout escape in the wasteland biome after the world evil has been killed.");
+            }
+        }
 
-public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-{
-	int
-		resourcesLayerIndex = layers.FindIndex(l => l.Name.Contains("Resource Bars")),
-		characterMenuIndex = layers.FindIndex(l => l.Name.Contains("Hotbar"));
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            int
+                resourcesLayerIndex = layers.FindIndex(l => l.Name.Contains("Resource Bars")),
+                characterMenuIndex = layers.FindIndex(l => l.Name.Contains("Hotbar"));
 
-	if (resourcesLayerIndex != -1)
-	{
-		layers.Insert(resourcesLayerIndex, new OverloadBarLayer());
-		layers.Insert(resourcesLayerIndex, new KiBarLayer());
-	}
-	if (characterMenuIndex != -1)
-	{
-		layers.Insert(characterMenuIndex, new CharacterTransformationsMenuLayer(characterTransformationsMenu, characterMenuInterface));
-		layers.Insert(characterMenuIndex, new KiBrowserLayer(kiBrowserMenu, kiBrowserInterface));
-	}
-}
-
-public static uint GetTicks() => Main.GameUpdateCount;
-
-public static bool IsTickRateElapsed(int rateModulo) => GetTicks() > 0 && GetTicks() % rateModulo == 0;
-
-
-internal static DBTMod Instance { get; private set; }
-}
+            if (resourcesLayerIndex != -1)
+            {
+                layers.Insert(resourcesLayerIndex, new OverloadBarLayer());
+                layers.Insert(resourcesLayerIndex, new KiBarLayer());
+            }
+            if (characterMenuIndex != -1)
+            {
+                layers.Insert(characterMenuIndex, new CharacterTransformationsMenuLayer(characterTransformationsMenu, characterMenuInterface));
+                layers.Insert(characterMenuIndex, new KiBrowserLayer(kiBrowserMenu, kiBrowserInterface));
+            }
+        }
+        public static uint GetTicks() => Main.GameUpdateCount;
+        public static bool IsTickRateElapsed(int rateModulo) => GetTicks() > 0 && GetTicks() % rateModulo == 0;
+        public static DBTMod Instance { get; set; }
+    }
 }
