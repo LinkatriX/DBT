4cb6df799a0a673644c4337921ecc4ca097e024e
Trusty Patches <38266369+Skipping903@users.noreply.github.com>
2019-07-30 16:06:44 -0500

    Updated file to fix conflict

--- NPCs/DBTGlobalNPC.cs
+++ NPCs/DBTGlobalNPC.cs
@@ -1,33 +1,30 @@
-﻿using DBT.Players;
+using DBT.Players;
 using Terraria;
 using Terraria.ModLoader;
-using Microsoft.Xna.Framework;
-using System.Collections.Generic;
 
 namespace DBT.NPCs
 {
-	public sealed class DBTGlobalNPC : GlobalNPC
-	{
-		public override void AI(NPC npc) //Need a method that is called every tick.
-		{
-			base.AI(npc);
-		}
+    public sealed class DBTGlobalNPC : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            DBTPlayer dbtPlayer = Main.LocalPlayer.GetModPlayer<DBTPlayer>();
 
-		public override void NPCLoot(NPC npc)
-		{
-			DBTPlayer dbtPlayer = Main.LocalPlayer.GetModPlayer<DBTPlayer>();
+            if (dbtPlayer == null)
+                return;
 
-			if (dbtPlayer == null)
-				return;
+            if (npc.lastInteraction == Main.LocalPlayer.whoAmI)
+                dbtPlayer.OnKilledNPC(npc);
+        }
 
-			if (npc.lastInteraction == Main.LocalPlayer.whoAmI)
-				dbtPlayer.OnKilledNPC(npc);
-		}
+        public override void GetChat(NPC npc, ref string chat)
+        {
+            DBTPlayer dbtPlayer = Main.LocalPlayer.GetModPlayer<DBTPlayer>();
 
-		public void DetectPlayerKills(NPC npc, DBTPlayer dbtPlayer)
-		{
-			//if (npc.lastInteraction == Main.LocalPlayer.whoAmI) //detecs wheter or not the recent NPC has communicated with the player.
-				
-		}
-	}
+            if (dbtPlayer.AliveTownNPCs.ContainsKey(npc))
+                dbtPlayer.AliveTownNPCs[npc] += 1;
+
+            base.GetChat(npc, ref chat);
+        }
+    }
 }
