3033f184d2932786ac51aeb8ab0536eb684d2c0e
Skipping903 <albieluk000@yahoo.com>
2019-09-10 01:59:08 -0500

    Started work on kk, extremely WIP

---
+++ Transformations/Kaiokens/Kaioken2x/Kaioken2xTransformation.cs
@@ -0,0 +1,34 @@
+﻿using DBT.Auras;
+using DBT.Players;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+
+namespace DBT.Transformations.Kaiokens.Kaioken2x
+{
+    public sealed class Kaioken2xTransformation : TransformationDefinition
+    {
+        public Kaioken2xTransformation(params TransformationDefinition[] parents) : base("Kaioken2x", "Kaioken 2x", typeof(Kaioken2xTransformationBuff),
+            1.05f, 1.025f, 1, new TransformationDrain(0f, 0f), new Kaioken2xTransformationAppearance(), new TransformationOverload(0, 0), 
+            displaysInMenu: false, parents: parents)
+        {
+        }
+    }
+
+    public sealed class Kaioken2xTransformationBuff : TransformationBuff
+    {
+        public Kaioken2xTransformationBuff() : base(TransformationDefinitionManager.Instance.Kaioken2x)
+        {
+        }
+    }
+
+    public sealed class Kaioken2xTransformationAppearance : TransformationAppearance
+    {
+        public Kaioken2xTransformationAppearance() : base(
+            new AuraAppearance(new AuraAnimationInformation("Auras/KaiokenAura", 4, 3, BlendState.Additive, 1f, true),
+                new LightingAppearance(new float[] { 0.35f, 0, 0 })),
+            new HairAppearance(Main.LocalPlayer.hairColor), Color.Red, Main.LocalPlayer.eyeColor)
+        {
+        }
+    }
+}
--- Transformations/TransformationDefinitionManager.cs
+++ Transformations/TransformationDefinitionManager.cs
@@ -18,6 +18,7 @@ using DBT.Transformations.SSJs.SSJ4s.SSJ4FP;
 using DBT.Transformations.Mystics.Mystic;
 using DBT.Transformations.Mystics.AwakenedMystic;
 using WebmilioCommons.Managers;
+using DBT.Transformations.Kaiokens.Kaioken2x;
 
 namespace DBT.Transformations
 {
@@ -25,6 +26,12 @@ namespace DBT.Transformations
     {
         public override void DefaultInitialize()
         {
+            Kaioken2x = Add(new Kaioken2xTransformation()) as Kaioken2xTransformation;
+            /*Kaioken3x = Add(new Kaioken3xTransformation()) as Kaioken3xTransformation;
+            Kaioken4x = Add(new Kaioken4xTransformation()) as Kaioken4xTransformation;
+            Kaioken10x = Add(new Kaioken10xTransformation()) as Kaioken10xTransformation;
+            Kaioken20x = Add(new Kaioken20xTransformation()) as Kaioken20xTransformation;*/
+
             SSJ1 = Add(new SSJ1Transformation()) as SSJ1Transformation;
             ASSJ1 = Add(new ASSJ1Transformation(SSJ1)) as ASSJ1Transformation;
             USSJ1 = Add(new USSJ1Transformation(ASSJ1)) as USSJ1Transformation;
@@ -60,6 +67,12 @@ namespace DBT.Transformations
         {
 
         }*/
+
+        public Kaioken2xTransformation Kaioken2x { get; private set; }
+        /*public Kaioken2xTransformation Kaioken3x { get; private set; }
+        public Kaioken2xTransformation Kaioken4x { get; private set; }
+        public Kaioken2xTransformation Kaioken10x { get; private set; }
+        public Kaioken2xTransformation Kaioken20x { get; private set; }*/
 
         public SSJ1Transformation SSJ1 { get; private set; }
         public ASSJ1Transformation ASSJ1 { get; private set; }
