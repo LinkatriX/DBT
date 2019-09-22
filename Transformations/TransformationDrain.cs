1fa114fca4f0d6c4f988c4f91a221bd39b642e10
Skipping903 <albieluk000@yahoo.com>
2019-09-22 14:59:32 -0500

    Finished SSJKK

--- Transformations/TransformationDrain.cs
+++ Transformations/TransformationDrain.cs
@@ -16,7 +16,7 @@
 
         public readonly int transformationStepDelay;
 
-        public TransformationDrain(float baseUnmasteredKiDrain, float baseMasteredKiDrain, float baseUnmasteredHealthDrain = 0f, float baseMasteredHealthDrain = 0f, 
+        public TransformationDrain(float baseUnmasteredKiDrain, float baseMasteredKiDrain = 0f, float baseUnmasteredHealthDrain = 0f, float baseMasteredHealthDrain = 0f, 
             int transformationStepDelay = 300, float maxTransformationKiDrainMultiplier = 3f, float kiMultiplierPerStep = 0.5f, float maxTransformationHealthDrainMultiplier = 3f, float healthMultiplierPerStep = 0)
         {
             this.baseUnmasteredKiDrain = baseUnmasteredKiDrain;
