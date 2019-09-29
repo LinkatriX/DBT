a6227926f663d4f4f9b0b54fe40e0089745eb58d
NuovaPrime <loganman51@hotmail.com>
2019-09-29 03:16:00 -0600

    :poop:

---
+++ Items/Weapons/MonkeyPooItem.cs
@@ -0,0 +1,33 @@
+﻿using DBT.Projectiles;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace DBT.Items.Weapons
+{
+    public sealed class MonkeyPooItem : DBTItem
+    {
+        public MonkeyPooItem() : base("Monkey Poo", "Literally just some monkey poo, why are you touching this?", 22, 32, Item.buyPrice(copper: 1), 0, ItemRarityID.Gray)
+        {
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            item.thrown = true;
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType<MonkeyPooProjectile>();
+            item.shootSpeed = 20f;
+            item.useTime = 12;
+            item.damage = 9;
+            item.useAnimation = 12;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.noMelee = true;
+            item.knockBack = 8f;
+            item.UseSound = SoundID.Item1;
+            item.maxStack = 9999;
+            item.consumable = true;
+        }
+    }
+}
---
+++ Projectiles/MonkeyPooProjectile.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using DBT.Helpers;
+
+namespace DBT.Projectiles
+{
+    public class MonkeyPooProjectile : ModProjectile
+    {
+    	public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Monkey Poo");
+		}
+    	
+        public override void SetDefaults()
+        {
+            projectile.width = 18;
+            projectile.height = 18;
+			projectile.light = 1f;
+            projectile.knockBack = 1.0f;
+            projectile.alpha = 0;
+            projectile.friendly = true;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = true;
+            projectile.netUpdate = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 120;
+			projectile.aiStyle = 2;
+
+			ProjectileID.Sets.TrailCacheLength[projectile.type] = 6;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            SoundHelper.PlayCustomSound("Sounds/Poop");
+            return true;
+        }
+
+        public override void AI()
+        {
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90f);
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			Vector2 drawOrigin = new Vector2(Main.projectileTexture[projectile.type].Width * 0.5f, projectile.height * 0.5f);
+
+			for (int k = 0; k < projectile.oldPos.Length; k++)
+			{
+				Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
+				Color color = projectile.GetAlpha(lightColor) * ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
+				spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, projectile.rotation, drawOrigin, projectile.scale, SpriteEffects.None, 0f);
+			}
+
+			return true;
+        }
+    }
+}
