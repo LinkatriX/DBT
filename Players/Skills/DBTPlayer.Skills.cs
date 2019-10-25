15244ae5ad7bf7c9e67f3f953348896160f10ea6
NuovaPrime <loganman51@hotmail.com>
2019-10-25 16:51:07 -0700

    Bit of misc skills work

--- Players/Skills/DBTPlayer.Skills.cs
+++ Players/Skills/DBTPlayer.Skills.cs
@@ -1,7 +1,9 @@
 ﻿using DBT.Helpers;
 using DBT.Skills;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ModLoader;
 
 namespace DBT.Players
 {
@@ -29,6 +31,43 @@ namespace DBT.Players
                     return true;
 
             return false;
+        }
+        public bool HasSkillActive(SkillDefinition definition)
+        {
+            for (int i = 0; i < ActiveSkills.Count; i++)
+                if (ActiveSkills.Contains(definition))
+                    return true;
+
+            return false;
+        }
+
+        public void AcquireSkill(SkillDefinition definition)
+        {
+            if (!HasAcquiredSkill(definition))
+            {
+                AcquiredSkills.Add(definition);
+                CombatText.NewText(new Rectangle((int)player.position.X, (int)player.position.Y, player.width, player.height), new Color(145, 228, 255), definition.DisplayName + " Unlocked!", true, false);
+            }
+                
+        }
+        public void EquipSkill(SkillDefinition definition)
+        {
+            if (!HasSkillActive(definition) && AcquiredSkills.Count < 4 && HasAcquiredSkill(definition))
+            {
+                ActiveSkills.Add(definition);
+                player.PutItemInInventory(mod.ItemType(definition.Item.Name));
+            }
+            if (HasSkillActive(definition))
+            {
+                ActiveSkills.Remove(definition);
+                foreach (var item in player.inventory)
+                {
+                    if (item == null)
+                        continue;
+                    if (item.type == mod.ItemType(definition.Item.Name))
+                        item.TurnToAir();
+                }
+            }
         }
 
         private void HandleSkillsOnEnterWorld(Player player)
