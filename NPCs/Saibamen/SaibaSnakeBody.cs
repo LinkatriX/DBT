8f47f4e1a87e053a369adad726493c3dec2a91dd
NuovaPrime <loganman51@hotmail.com>
2019-11-25 00:39:45 -0800

    Broke ass worms :flushed:

---
+++ NPCs/Saibamen/SaibaSnakeBody.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBT.NPCs.Saibamen
+{
+    public class SaibaSnakeBody : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Snakeaman");
+        }
+        public override void SetDefaults()
+        {
+            npc.CloneDefaults(NPCID.DiggerBody);
+            npc.aiStyle = -1;
+        }
+    }
+}
